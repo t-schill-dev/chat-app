8f90f3e2df5e09d4b6bff94f9db108aad11ead66
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-07-27 09:28:39 +0200

    Add custom view for map

--- Components/Chat.js
+++ Components/Chat.js
@@ -179,6 +179,24 @@ export default function Chat({ route, navigation }) {
     return <CustomActions {...props}/>
   };
 
+  const renderCustomView = (props)=> {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      );
+    }
+    return null;
+  }
+
   //Changing color by inheriting props of function
   const renderBubble = function (props) {
     return (
@@ -226,6 +244,7 @@ export default function Chat({ route, navigation }) {
         renderSend={renderSend}
         renderBubble={renderBubble}
         renderActions={renderCustomActions}
+        renderCustomView={renderCustomView}
         messages={messages}
         onSend={(messages) => onSend(messages)}
         renderInputToolbar={renderInputToolbar}
